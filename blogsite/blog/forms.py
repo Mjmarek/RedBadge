2e1aed4bd20692c69b25a5999199e5ff1bb1ed9a
Mjmarek <monicamarek24@gmail.com>
2018-01-09 13:03:04 -0500

    modified files for PyCharm

--- blogsite/blog/forms.py
+++ blogsite/blog/forms.py
@@ -1,22 +1,24 @@
 from django import forms
-from .models import Post,Comment
+from .models import Post, Comment
+
 
 class PostForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = Post
-        fields = ('author','title','text')
+        fields = ('author', 'title', 'text')
 
         widgets = {
-            'title':forms.TextInput(attrs={'class':'textinputclass'}),
-            'text':forms.Textarea(attrs={'class':'postcontent'})
+            'title': forms.TextInput(attrs={'class': 'text_input_class'}),
+            'text': forms.Textarea(attrs={'class': 'post_content'})
         }
 
+
 class CommentForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = Comment
-        fields = ('author','text')
+        fields = ('author', 'text')
 
         widgets = {
-            'author':forms.TextInput(attrs={'class':'textinputclass'}),
-            'text':forms.Textarea(attrs={'class':'commentcontent'})
+            'author': forms.TextInput(attrs={'class': 'text_input_class'}),
+            'text': forms.Textarea(attrs={'class': 'comment_content'})
         }
--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=100)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True,null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,13 +18,14 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse("post_detail",kwargs={'pk':self.pk})
+        return reverse('post_detail', kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=100)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now())
