31d3baccc2a6b8662dcc59433d1938e116a1e770
adsieg01 <adsieg01@gmail.com>
2018-01-17 09:48:15 -0500

    added video funcitonality

--- blogsite/blog/admin.py
+++ blogsite/blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post, Comment
+from embed_video.admin import AdminVideoMixin
+
+
+class PostAdmin(AdminVideoMixin, admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Post)
+
 admin.site.register(Comment)
