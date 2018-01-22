da1a8b8dc8d69100d56b72844d2a5d110f3bc26a
Mjmarek <monicamarek24@gmail.com>
2018-01-22 14:11:14 -0500

    added Pipfile.log & configured static

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -20,6 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
 SECRET_KEY = 'mxl&nc+snzyobo*ymse^as-6m+^x_u0c!^0(#dsibyiw#n1ay6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -78,11 +79,10 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todahblog',
+        'NAME': os.environ.get("NAME_PASSWORD"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        'USER': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'USER': os.environ.get("USER_PASSWORD"),
+        'HOST': os.environ.get("HOST_PASSWORD"),
     }
 }
 
